431d4d2d9c10aa66a9348e5d66ebde1e9e134527
Felipe Campos Penha <fcpenha@users.noreply.github.com>
2017-02-27 10:48:59 -0300

    Add files via upload

---
+++ titanic_survival_exploration/run.sh
@@ -0,0 +1,21 @@
+# REALLY IMPORTANT code snippet, for ignoring 
+# python's user site libraries which are
+# (usually previously installed)
+export PYTHONNOUSERSITE=1
+
+export PATH="/home/fcpenha/Work/python_collection/miniconda/bin:$PATH"
+
+conda create -n titanic_env --file conda_req.txt
+
+source activate titanic_env
+
+# Execute and replace notebook
+jupyter nbconvert --execute --to notebook --inplace titanic_survival_exploration.ipynb
+
+# Export as a HTML with the name 'report.html'
+jupyter nbconvert --to html titanic_survival_exploration.ipynb --output report
+
+# Open up jupyter
+jupyter notebook titanic_survival_exploration.ipynb
+
+source deactivate
